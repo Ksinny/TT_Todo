befec29ec9db82240c07fac09ede4f94f71debf4
Ksinny <dosly2@naver.com>
2025-12-17 22:29:24 +0900

    chore: 공통 API 응답용BasicResponseDto 정의

---
+++ src/main/java/TaskTrek/todo/dto/BasicResponseDto.java
@@ -0,0 +1,29 @@
+package TaskTrek.todo.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class BasicResponseDto<T> {
+    private final boolean success;
+    private final String message;
+    private final T data;
+
+
+    // 성공 응답 (데이터 포함)
+    public static <T> BasicResponseDto<T> success(T data, String message) {
+        return new BasicResponseDto<>(true, message, data);
+    }
+
+    // 성공 응답
+    public static <T> BasicResponseDto<T> success(String message) {
+        return new BasicResponseDto<>(true, message, null);
+    }
+
+    // 실패 응답
+    public static BasicResponseDto<Object> fail(String message) {
+        return new BasicResponseDto<>(false, message, null);
+    }
+}
