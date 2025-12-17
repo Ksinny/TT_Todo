ca23d5d445e0e52719622dee947f1b6bcc7723dd
Ksinny <dosly2@naver.com>
2025-12-18 03:50:14 +0900

    chore: 예외 처리를 위한 ErrorCode 및 CustomException 정의

---
+++ src/main/java/TaskTrek/todo/exception/ErrorCode.java
@@ -0,0 +1,21 @@
+package TaskTrek.todo.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    // User
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
+    DUPLICATE_EMAIL(HttpStatus.CONFLICT, "이미 가입된 이메일입니다."),
+    FAIL_LOGIN(HttpStatus.UNAUTHORIZED, "아이디 또는 비밀번호를 잘못 입력했습니다."),
+
+    // Common
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 에러가 발생했습니다."),
+    INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "잘못된 입력값입니다.");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}
