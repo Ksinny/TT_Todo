ba5abf25af026a997a38fb33c7a63edb764aae36
Ksinny <dosly2@naver.com>
2025-12-17 22:51:30 +0900

    feat: User 요청 및 응답 DTO 구현
    - 회원가입, 로그인, 이메일 인증 관련 요청

---
+++ src/main/java/TaskTrek/todo/dto/UserRequestDto.java
@@ -0,0 +1,101 @@
+package TaskTrek.todo.dto;
+
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import TaskTrek.todo.entity.User;
+
+public class UserRequestDto {
+
+    /**
+     * 회원가입 요청 DTO
+     */
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class Signup {
+
+        @NotBlank(message = "이메일을 입력해주세요.")
+        @Email(message = "유효하지 않은 이메일입니다.")
+        @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$",
+                message = "유효하지 않은 이메일입니다.")
+        private String email;
+
+        @NotBlank(message = "비밀번호를 입력해주세요.")
+        @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$",
+                message = "영문과 숫자를 포함하여 8자 이상의 비밀번호를 입력해주세요.")
+        private String password;
+
+        @NotBlank(message = "비밀번호 확인을 입력해주세요.")
+        private String passwordConfirm;
+
+        @NotBlank(message = "이름을 입력해주세요.")
+        private String name;
+
+        // 비밀번호 일치 여부 확인 로직
+        @AssertTrue(message = "비밀번호가 일치하지 않습니다.")
+        public boolean isPasswordConfirmed() {
+            return password != null && password.equals(passwordConfirm);
+        }
+
+        public Signup(String email, String password, String name) {
+            this.email = email;
+            this.password = password;
+            this.name = name;
+        }
+
+        // DTO를 엔티티로 변환
+        public static User toEntity(Signup dto) {
+            return User.builder()
+                    .email(dto.getEmail())
+                    .password(dto.getPassword())
+                    .name(dto.getName())
+                    .build();
+        }
+    }
+
+    /**
+     * 로그인 요청 DTO
+     */
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class Login {
+
+        @NotBlank(message = "이메일을 입력해주세요.")
+        @Email(message = "유효하지 않은 이메일입니다.")
+        private String email;
+
+        @NotBlank(message = "비밀번호를 입력해주세요.")
+        private String password;
+    }
+
+    /**
+     * 이메일 인증번호 발송 요청 DTO
+     */
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class EmailCheck {
+
+        @NotBlank(message = "이메일을 입력해주세요.")
+        @Email(message = "유효하지 않은 이메일입니다.")
+        private String email;
+    }
+
+    /**
+     * 이메일 인증번호 검증 요청 DTO
+     */
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class EmailAuth {
+
+        @NotBlank(message = "이메일을 입력해주세요.")
+        @Email(message = "유효하지 않은 이메일입니다.")
+        private String email;
+
+        @NotBlank(message = "인증 코드를 입력해주세요.")
+        private String code;
+    }
+}
---
+++ src/main/java/TaskTrek/todo/dto/UserResponseDto.java
@@ -0,0 +1,32 @@
+package TaskTrek.todo.dto;
+
+import TaskTrek.todo.entity.User;
+import lombok.*;
+
+public class UserResponseDto {
+
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class Response {
+        private Long id;
+        private String email;
+        private String name;
+
+
+        @Builder
+        public Response(Long id, String email, String name) {
+            this.id = id;
+            this.email = email;
+            this.name = name;
+        }
+
+        // 엔티티를 DTO로 변환
+        public static Response from(User user) {
+            return Response.builder()
+                    .id(user.getId())
+                    .email(user.getEmail())
+                    .name(user.getName())
+                    .build();
+        }
+    }
+}
